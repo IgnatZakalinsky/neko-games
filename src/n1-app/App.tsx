1b524e123ff5c2bfd5a213aef59495c793036516
ai73a@yandex.ru <1qazxcvBG>
2021-02-25 00:52:28 +0300

    start

---
+++ src/n1-app/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './App.module.css'
+
+const App = () => {
+    return (
+        <div className={s.app}>
+            <header>
+                Neko Games
+            </header>
+        </div>
+    )
+}
+
+export default App
